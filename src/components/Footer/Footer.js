ff2ca0a196461d403a81f088aa83613236d6aa82
juanospina0409 <ospinajuan0409@gmail.com>
2026-01-20 11:44:04 -0500

    Reapply "(#22) Re-estructuracion de la estandarizacion de nombres actualizado"
    This reverts commit 1a2e5fbcb2815cbf054123fe91c8f58921593198.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+﻿// permite mostrar la barra de navegacion
+export default class Footer extends HTMLElement {
+  async connectedCallback() {
+    // Cargar el HTML
+    const resp = await fetch("../components/Footer/Footer.html");
+    const html = await resp.text();
+    this.innerHTML = html;
+  }
+}
+
+customElements.define("footer-page", Footer);
+
+
