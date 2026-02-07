66ec5bb01a3592dc440de8d135ed1c712cf95eea
Crisstiv <160662456+Crisstiv@users.noreply.github.com>
2026-02-06 21:05:26 -0500

    Caso#33 (#39)
    * Se realiza el cambio en el archivo servicioscorusel.js
    
    * Se agrega descripcion dentro del cadigo servicioscarusel de lo que se cambio en el codigo

--- src/scripts/servicioscorusel.js
+++ src/scripts/servicioscorusel.js
@@ -1,5 +1,5 @@
 const track = document.querySelector("#hero-carousel2 .carousel-track");
-let speed = -0.4; // 👈 controla la elegancia (0.2 muy lento, 0.6 normal), negativo para izquierda
+let speed = -1.4; // 👈 controla la elegancia (0.2 muy lento, 0.6 normal), negativo para izquierda
 let position = 0;
 
 // Duplicamos el contenido para loop infinito
@@ -10,11 +10,11 @@ const trackWidth = track.scrollWidth / 2;
 function animate() {
   position += speed;
 
-  // cuando llega a la mitad, invierte la dirección
+  // bucle de de la infromacion del carrusel
   if (position <= -trackWidth) {
-    speed = -speed;
-  } else if (position >= trackWidth) {
-    speed = -speed;
+    position = 0;
+  } else if (position > 0) {
+    position = -trackWidth;
   }
 
   track.style.transform = `translateX(${position}px)`;
