ed05aea960c1b87c5bd2a2660a286ffda3df6d7b
juanospina0409 <ospinajuan0409@gmail.com>
2026-02-18 20:46:50 -0500

    (#46) - Implementación: Se necesita crear componente web del formulario

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-﻿// permite mostrar la barra de navegacion
+﻿// permite mostrar el pie de pagina
 export default class Footer extends HTMLElement {
   async connectedCallback() {
     // Cargar el HTML
