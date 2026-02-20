fac7b65b23bebc6850d8c164e7b99f47f3dc91d4
DiegoWhite99 <diegofer.cas.99@gmail.com>
2026-02-19 20:00:38 -0500

    se realiza cambios desde BaseComponentes.js por rutas mas estrictas caso adigando al #46

--- src/components/BaseComponents.js
+++ src/components/BaseComponents.js
@@ -1,4 +1,4 @@
-﻿import './NavigationBar/NavigationBar.js';
-import './Footer/Footer.js';
-import './Form/Form.js';
-import '/backend/form/formHandler.js';
+﻿import '../components/NavigationBar/NavigationBar.js';
+import '../components/Footer/Footer.js';
+import '../components/Form/Form.js';
+
